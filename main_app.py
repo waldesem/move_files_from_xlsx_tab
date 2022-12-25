44eda0f8fb85eff934a2a15fa4736689c41df973
Vladimir Semenenko <wsemenenko@gmail.com>
2022-12-25 17:10:48 +0300

    Many fixes

--- main_app.py
+++ main_app.py
@@ -1,18 +1,17 @@
 import shutil
 import sqlite3
-import sys
 import os
 from datetime import date, datetime
 
 import openpyxl
 
 
-DATE = date.today()
-# CONNECT = r'\\cronosx1\New folder\УВБ\Отдел корпоративной защиты\candidates.db' #     файл базы данных
-# WORK_DIR = r'\\cronosx1\New folder\УВБ\Отдел корпоративной защиты\Кандидаты\\'    # рабочая папка кандидатов
-# DESTINATION = r'\\cronosx1\New folder\УВБ\Отдел корпоративной защиты\Персонал\Персонал-2\\'   # архивная папка
-# INFO_FILE = r'\\cronosx1\New folder\УВБ\Отдел корпоративной защиты\Кандидаты\Запросы по работникам.xlsx'  # запросы
-# MAIN_FILE = r'\\cronosx1\New folder\УВБ\Отдел корпоративной защиты\Кандидаты\Кандидаты.xlsm'  # файл кандидатов
+DATE = date.today() # сегодняшняя дата (объект)
+CONNECT = r'\\cronosx1\New folder\УВБ\Отдел корпоративной защиты\candidates.db' #     файл базы данных
+WORK_DIR = r'\\cronosx1\New folder\УВБ\Отдел корпоративной защиты\Кандидаты\\'    # рабочая папка кандидатов
+DESTINATION = r'\\cronosx1\New folder\УВБ\Отдел корпоративной защиты\Персонал\Персонал-2\\'   # архивная папка
+INFO_FILE = r'\\cronosx1\New folder\УВБ\Отдел корпоративной защиты\Кандидаты\Запросы по работникам.xlsx'  # запросы
+MAIN_FILE = r'\\cronosx1\New folder\УВБ\Отдел корпоративной защиты\Кандидаты\Кандидаты.xlsm'  # файл кандидатов
 # поля в таблице candidates базы данных candidates:
 SQL_CAND = 'staff, department, full_name, last_name, birthday, birth_place, country, series_passport, ' \
            'number_passport, date_given, snils, inn, reg_address, live_address, phone, email, education, ' \
@@ -21,21 +20,16 @@ SQL_CAND = 'staff, department, full_name, last_name, birthday, birth_place, coun
 # поля в таблице registry базы данных candidates:
 SQL_REG = 'fio, birthday, staff, checks, recruiter, date_in, officer, date_out, result, final_date, url'
 # поля в таблице inquiry базы данных candidates:
-SQL_INQ = '???????????????????????????'
-# тестовые константы:
-CONNECT = r'C:\Users\ubuntu\Documents\Отдел корпоративной защиты\candidates.db'  #     файл базы данных
-WORK_DIR = r'C:\Users\ubuntu\Documents\Отдел корпоративной защиты\Кандидаты\\'  # рабочая папка
-DESTINATION = r'C:\Users\ubuntu\Documents\Отдел корпоративной защиты\Персонал\Персонал-2\\' # архивная папка
-MAIN_FILE = r'C:\Users\ubuntu\Documents\Отдел корпоративной защиты\Кандидаты\Кандидаты.xlsm'   # файл кандидатов
-INFO_FILE = r'C:\Users\ubuntu\Documents\Отдел корпоративной защиты\Кандидаты\Запросы по работникам.xlsx' # запросы
+SQL_INQ = 'full_name, birthday, staff, period, info, firm, date_inq'
+# тестовые:
+# CONNECT = r'C:\Users\ubuntu\Documents\Отдел корпоративной защиты\candidates.db'  #     файл базы данных
+# WORK_DIR = r'C:\Users\ubuntu\Documents\Отдел корпоративной защиты\Кандидаты\\'  # рабочая папка
+# DESTINATION = r'C:\Users\ubuntu\Documents\Отдел корпоративной защиты\Персонал\Персонал-2\\' # архивная папка
+# MAIN_FILE = r'C:\Users\ubuntu\Documents\Отдел корпоративной защиты\Кандидаты\Кандидаты.xlsm'   # файл кандидатов
+# INFO_FILE = r'C:\Users\ubuntu\Documents\Отдел корпоративной защиты\Кандидаты\Запросы по работникам.xlsx' # запросы
 
 
-def backup(filename) -> None:    #  резервные копии файлов
-    for file in [CONNECT]+filename:
-        shutil.copy(file, DESTINATION)
-    print('Резервное копирование завершено')
-
-def range_row(book, sheet) -> list: # получаем список номеров строк соответствующих сегодняшней дате
+def range_row(sheet) -> list: # получаем список номеров строк соответствующих сегодняшней дате
     row_num = []
     for cell in sheet:
         for c in cell:
@@ -44,46 +38,29 @@ def range_row(book, sheet) -> list: # получаем список номеро
                     row_num.append(c.row)
             elif str(c.value).strip() == DATE.strftime('%d.%m.%Y'):
                 row_num.append(c.row)
-    if len(row_num) == 0:
-        print('Данные за сегодня отсутствуют')
-        book.close()
-        sys.exit()
-    print('Получен список строк')
     return row_num
 
 def dir_range(sheet, row_num) -> list:  # получаем список папок в рабочей директории
     fio = [sheet['B' + str(i)].value.strip().lower() for i in row_num]
     subdir = [sub for sub in os.listdir(WORK_DIR) if sub.lower().strip() in fio]
-    if len(subdir) == 0: # если папок соответствующих записям в файле нет - передаем записи из реестра в БД
-        registry_check(row_num, sheet)
-        print('Папки в рабочей директории не найдены')
-        sys.exit()
-    print('Создан список папок из рабочей директории')
     return subdir
 
-def file_range(book, sheet, subdir, row_num) -> list: # получаем список путей к файлам Заключений
+def file_range(subdir) -> list: # получаем список путей к файлам Заключений
     name_path = list(filter(None, []))
     for f in subdir:
         subdir_path = f"{WORK_DIR[0:-1] + f}\\"
         for file_name in os.listdir(subdir_path):
             if file_name.startswith("Заключение") and file_name.endswith("xlsm"):
                 name_path.append(os.path.join(WORK_DIR, subdir_path, file_name))
-    if len(name_path) == 0: # если файлов соответствующих шаблону нет, перемещаем папки, передаем данные в БД
-        print('Файлы заключений не найдены')
-        create_link(sheet, subdir, row_num)
-        registry_check(row_num, sheet)
-        book.save(MAIN_FILE)
-        sys.exit()
-    print('Создан список файлов с заключениями')
     return name_path
 
-def check_types(check) -> list: # проверка и преобразование типов данных
+def check_types(check) -> list: # преобразование типов данных
     view = []
     for c in check:
         if isinstance(c, datetime):  # преобразование datetime в строку
             view.append(c.strftime('%Y-%m-%d'))
         else:
-            view.append(str(c))    # преобразование в строку
+            view.append(str(c).strip())    # преобразование в строку
     return view
 
 def parse_excel(path_files) -> list: # получаем список кортежей с данными заключений
@@ -117,15 +94,14 @@ def parse_excel(path_files) -> list: # получаем список корте�
     return conclusion
 
 def create_link(sheet, subdir, row_num) -> None:    # создаем гиперссылки и перемещаем папки
-    for nums in row_num:
+    for n in row_num:
         for sub in subdir:
-            if str(sheet['B' + str(nums)].value.strip().lower()) == sub.strip().lower():
-                sbd = sheet['B' + str(nums)].value.strip()
-                lnk = f"{DESTINATION + sbd[0][0]}\\{sbd} - {sheet['A' + str(nums)].value}"
-                sheet['L' + str(nums)].hyperlink = lnk    # записывает в книгу
+            if str(sheet['B' + str(n)].value.strip().lower()) == sub.strip().lower():
+                sbd = sheet['B' + str(n)].value.strip()
+                lnk = f"{DESTINATION + sbd[0][0]}\\{sbd} - {sheet['A' + str(n)].value}"
+                sheet['L' + str(n)].hyperlink = lnk    # записывает в книгу
                 shutil.move(WORK_DIR + sbd, lnk)
-    print('Гиперссылки созданы и записаны')
-    print('Папки перемещены в архив')
+    print('Гиперссылки записаны, папки перемещены в архив')
 
 def insert_db(query, value) -> None:  # запись в БД
     with sqlite3.connect(CONNECT, timeout=5.0) as con:
@@ -144,45 +120,49 @@ def registry_check(row_num, sheet) -> None:  # Получаем данные и�
 def inquiry_check() -> None:  # Получаем данные из реестра запросов и передаем в БД
     wbc = openpyxl.load_workbook(INFO_FILE, keep_vba=True)
     wsc = wbc.worksheets[0]   # открываем первый лист книги MAIN_FILE для чтения и записи данных
-    row_num = range_row(wbc, wsc['K5000':'K20000']) # ?????????????????????????????
-    inquiry_val = []
-    for n in row_num: # получаем список значений строк соответствующих сегодняшней дате
-        inquiry_val.append(tuple(check_types([c.value for cell in wsc['B' + str(n):'L' + str(n)] for c in cell]))) #???
-    print('Получена информация из реестра запросов')
-    insert_db("INSERT INTO inquiry ({}) VALUES ({}?)".format(SQL_INQ, '?, ' * (len(SQL_INQ.split()) - 1)), inquiry_val)
+    row_num = range_row(wsc['G1':'G2000'])
+    if len(row_num) > 0:
+        print('Данные за сегодня найдены')
+        inquiry_val = []
+        for n in row_num:  # получаем список значений строк соответствующих сегодняшней дате
+            inquiry_val.append(tuple(check_types([c.value for cell in wsc['A' + str(n):'G' + str(n)] for c in cell])))
+        print('Получена информация из реестра запросов')
+        inquiry_query = "INSERT INTO inquiry ({}) VALUES ({}?)".format(SQL_INQ, '?, ' * (len(SQL_INQ.split()) - 1))
+        insert_db(inquiry_query, inquiry_val)
     wbc.close()  # Закрываем книгу Excel
 
-def check_cand():   # разбор реестра Кандидаты и Заключений
+def cand_check() -> None:   # разбор реестра Кандидаты и Заключений
     wb = openpyxl.load_workbook(MAIN_FILE, keep_vba=True, read_only=False)
     ws = wb.worksheets[0]   # открываем первый лист книги MAIN_FILE для чтения и записи данных
-    num_row = range_row(wb, ws['K5000':'K20000']) # записываем номера строк соответствующих сегодняшней дате
-    subdirectory = dir_range(ws, num_row)   # список директорий, которые соответствуют фамилиям кандидатов
-    ins_cand = "INSERT INTO candidates ({}) VALUES ({}?)".format(SQL_CAND, '?, ' * (len(SQL_CAND.split()) - 1))
-    insert_db(ins_cand, parse_excel(file_range(wb, ws, subdirectory, num_row)))   # передаем данные в БД
-    create_link(ws, subdirectory, num_row)  # Создание гиперссылки и перемещение папок
-    registry_check(num_row, ws) # получаем данные из реестра и передаем в БД
-    wb.save(MAIN_FILE)  # Сохраняем книгу Excel
+    num_row = range_row(ws['K5000':'K20000']) # записываем номера строк соответствующих сегодняшней дате
+    if len(num_row) == 0:
+        print('Данные за сегодня отсутствуют')
+        wb.close()  # Закрываем книгу Excel
+    else:
+        print('Получен список строк из книги')
+        subdirectory = dir_range(ws, num_row)   # список директорий, которые соответствуют фамилиям кандидатов
+        if len(subdirectory) > 0:
+            print('Создан список папок из рабочей директории')
+            path_files = file_range(subdirectory)
+            if len(path_files) > 0:
+                print('Файлы заключений найдены')
+                ins_cand = "INSERT INTO candidates ({}) VALUES ({}?)".format(SQL_CAND, '?, ' * (len(SQL_CAND.split()) - 1))
+                insert_db(ins_cand, parse_excel(path_files))  # передаем данные в БД
+            create_link(ws, subdirectory, num_row)  # Создание гиперссылки и перемещение папок
+        registry_check(num_row, ws)  # получаем данные из реестра и передаем в БД
+        wb.save(MAIN_FILE)  # Сохраняем книгу Excel
 
-def main(): # проверка файлов на изменения с датой сегодня
+def main(): # проверка файлов на изменения с датой сегодня, тип запуска программы в зависимости от результата
     main_file_date = date.fromtimestamp(os.path.getmtime(MAIN_FILE))
     info_file_date = date.fromtimestamp(os.path.getmtime(INFO_FILE))
-    if main_file_date != DATE and info_file_date != DATE:
-        print('Файлы MAIN_FILE и INFO_FILE не изменялись')
-        sys.exit()
-    elif main_file_date == DATE and info_file_date != DATE:
-        print('Файл MAIN_FILE изменялся')
-        backup([MAIN_FILE])
-        check_cand()
-    elif main_file_date != DATE and info_file_date == DATE:
-        print('Файл INFO_FILE изменялся')
-        backup([INFO_FILE])
+    if main_file_date == DATE or info_file_date == DATE:
+        shutil.copy(CONNECT, DESTINATION)  # backup files
+    if main_file_date == DATE:
+        shutil.copy(MAIN_FILE, DESTINATION)
+        cand_check()
+    if info_file_date == DATE:
+        shutil.copy(INFO_FILE, DESTINATION)
         inquiry_check()
-    else:
-        print('Файлы MAIN_FILE и INFO_FILE были изменены')
-        backup([MAIN_FILE, INFO_FILE])
-        check_cand()
-        inquiry_check()
-
 
 if __name__ == "__main__":
     main()
